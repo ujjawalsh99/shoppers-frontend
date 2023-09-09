cb3c8f90fcd8102fb24326e77151354ce31d2877
Ujjawal Shrivastava <ujjawalsh99@gmail.com>
2023-09-09 22:38:19 +0530

    Add CanActivate guard

--- src/app/guard.service.ts
+++ src/app/guard.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './service/auth.service';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class GuardService {
-
-  constructor() { }
+@Injectable()
+export class GuardService implements CanActivate {
+  canActivate(
+    route: ActivatedRouteSnapshot
+  ): boolean | Observable<boolean> | Promise<boolean> {
+    if (!this.authService.isLoggedIn()) {
+      this.router.navigate(['/login']); // go to login if not authenticated
+      return false;
+    }
+    return true;
+  }
+  constructor(private authService: AuthService, private router: Router) { }
 }
